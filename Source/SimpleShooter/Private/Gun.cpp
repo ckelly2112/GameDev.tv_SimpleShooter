267c440456bcbf978aed08dd5a5f44731812a060
Christopher Kelly <ckelly28@myseneca.ca>
2020-06-29 11:46:13 -0400

    Created a gun class and made a rifle blueprint

---
+++ Source/SimpleShooter/Private/Gun.cpp
@@ -0,0 +1,34 @@
+// Please visit www.chriskelly48th.ca for contact info
+
+
+#include "Gun.h"
+#include "Components/SkeletalMeshComponent.h"
+
+// Sets default values
+AGun::AGun()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	SetRootComponent(Root);
+
+	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Gun Mesh"));
+	Mesh->SetupAttachment(Root);
+
+}
+
+// Called when the game starts or when spawned
+void AGun::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGun::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
