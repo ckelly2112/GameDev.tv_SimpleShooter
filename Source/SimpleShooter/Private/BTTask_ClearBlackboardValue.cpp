f9a0a8a7b20710658c2865c01d1cac26bbe71d7a
Christopher Kelly <ckelly28@myseneca.ca>
2020-07-10 15:07:02 -0400

    AI now clears last known value after investigation

---
+++ Source/SimpleShooter/Private/BTTask_ClearBlackboardValue.cpp
@@ -0,0 +1,19 @@
+// Please visit www.chriskelly48th.ca for contact info
+
+
+#include "BTTask_ClearBlackboardValue.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTTask_ClearBlackboardValue::UBTTask_ClearBlackboardValue()
+{
+	NodeName = TEXT("Clear Blackboard Value");
+}
+
+
+EBTNodeResult::Type UBTTask_ClearBlackboardValue::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	Super::ExecuteTask(OwnerComp, NodeMemory);
+	OwnerComp.GetBlackboardComponent()->ClearValue(GetSelectedBlackboardKey());
+	return EBTNodeResult::Succeeded;
+}
+
---
+++ Source/SimpleShooter/Public/BTTask_ClearBlackboardValue.h
@@ -0,0 +1,26 @@
+// Please visit www.chriskelly48th.ca for contact info
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BTTask_ClearBlackboardValue.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLESHOOTER_API UBTTask_ClearBlackboardValue : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+public:
+	UBTTask_ClearBlackboardValue();
+
+protected:
+
+private:
+
+	
+};
