c63ebe62d7438974c73922399e86b19525059b63
Christopher Kelly <ckelly28@myseneca.ca>
2020-07-12 10:32:10 -0400

    Implementing Player Location Service

---
+++ Source/SimpleShooter/Private/BTService_PlayerLocation.cpp
@@ -0,0 +1,5 @@
+// Please visit www.chriskelly48th.ca for contact info
+
+
+#include "BTService_PlayerLocation.h"
+
---
+++ Source/SimpleShooter/Public/BTService_PlayerLocation.h
@@ -0,0 +1,18 @@
+// Please visit www.chriskelly48th.ca for contact info
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Services/BTService_BlackboardBase.h"
+#include "BTService_PlayerLocation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLESHOOTER_API UBTService_PlayerLocation : public UBTService_BlackboardBase
+{
+	GENERATED_BODY()
+	
+
+};
